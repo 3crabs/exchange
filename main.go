21ad303d158d2d88270677263959b1427b9ac223
user-penguin <kobzev96@gmail.com>
2022-01-22 02:38:54 +0700

    add gin stub for /

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"messaage": "Hello my little friends",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		panic(err)
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
